ab93e509ae25b62b5fafcae7b3d34b532d0f0657
junayed21 <b82854370@gmail.com>
2024-10-29 23:47:01 +0600

    Update customrankcard.js

--- scripts/cmds/customrankcard.js
+++ scripts/cmds/customrankcard.js
@@ -6,7 +6,7 @@ const { uploadImgbb } = global.utils;
 module.exports = {
 	config: {
 		name: "customrankcard",
-		aliases: ["crc2", "customrank"],
+		aliases: ["crc", "customrank"],
 		version: "1.12",
 		author: "NTKhang",
 		countDown: 5,
